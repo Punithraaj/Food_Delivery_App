76b58b822e24294c706f731ffba08785102345ce
Punithraj <punithraj14@gmail.com>
2020-12-30 12:12:31 +0530

    Food Delivery App

---
+++ lib/models/order.dart
@@ -0,0 +1,11 @@
+import 'package:food_delivery_app/models/food.dart';
+import 'package:food_delivery_app/models/restaurant.dart';
+
+class Order {
+  final Restaurant restaurant;
+  final Food food;
+  final String date;
+  final int quantity;
+
+  Order(this.restaurant, this.food, this.date, this.quantity);
+}
